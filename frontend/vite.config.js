1add9f5cc9108d2a649a0464520679de1952051b
shader006 <shader006@users.noreply.github.com>
2026-09-24 21:15:03 +0700

    fix(frontend): remove envDir to keep frontend/.env completely independent

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  envDir: path.resolve(__dirname, '..'),
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
